b51f5145b903223c4a986a88273f60ad3c413db7
Temur Uzbekov <uzbekov.temur@gmail.com>
2018-10-20 22:56:10 +0500

    add proper worker pool startup

--- src/worker.py
+++ src/worker.py
@@ -1,7 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 import time
 import random
-import threading
 from database.common import execute
 from database.queries import TAKE_TASK, RUN_TASK_OR_GIVE_AWAY, FINISH_TASK
 from logger import get_logger
@@ -19,18 +18,18 @@ def test_task():
     time.sleep(random.randint(TEST_TASK_SLEEP_MIN, TEST_TASK_SLEEP_MAX))
 
 
-def worker():
-    worker_id = threading.get_ident()
+def worker(worker_id):
+    """ Main worker function. Takes tasks from the queue and executes them """
+    LOGGER.info(f'Worker {worker_id} launched')
     while True:
-        LOGGER.info(f'Worker launched: {worker_id}')
         test_task()
         LOGGER.info(f'Task finished by worker: {worker_id}')
 
 
 def main():
     with ThreadPoolExecutor(max_workers=WORKERS_CNT) as executor:
-        executor.submit(worker)
-        executor.submit(worker)
+        for idx in range(WORKERS_CNT):
+            executor.submit(worker, idx)
 
 
 if __name__ == '__main__':
